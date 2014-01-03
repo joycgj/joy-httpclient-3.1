ba4fd14ddc2d61f32bfe09f568a7dd65f69f0327
gaojiechen <gaojiechen@sohu-inc.com>
2014-01-03 10:56:09 +0800

    [httpclient company]

--- httpclient/src/main/java/org/apache/commons/httpclient/params/DefaultHttpParams.java
+++ httpclient/src/main/java/org/apache/commons/httpclient/params/DefaultHttpParams.java
@@ -1,16 +1,52 @@
-package org.apache.commons.httpclient.params;
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//httpclient/src/java/org/apache/commons/httpclient/params/DefaultHttpParams.java,v 1.9 2004/12/21 23:15:21 olegk Exp $
+ * $Revision: 510589 $
+ * $Date: 2007-02-22 18:04:52 +0100 (Thu, 22 Feb 2007) $
+ *
+ * ====================================================================
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+package org.apache.commons.httpclient.params;
 
 import java.io.Serializable;
 import java.util.HashMap;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * This class represents a collection of HTTP protocol parameters. Protocol parameters
- * may be linked together to from a hierarchy. If a particular parameter value has not been
+ * may be linked together to form a hierarchy. If a particular parameter value has not been
  * explicitly defined in the collection itself, its value will be drawn from the parent
  * collection of parameters.
+ *
+ * @author <a href="mailto:oleg@ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision: 510589 $
+ *
+ * @since 3.0
  */
 public class DefaultHttpParams implements HttpParams, Serializable, Cloneable {
 
--- httpclient/src/main/java/org/apache/commons/httpclient/params/HttpParams.java
+++ httpclient/src/main/java/org/apache/commons/httpclient/params/HttpParams.java
@@ -1,3 +1,33 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//httpclient/src/java/org/apache/commons/httpclient/params/HttpParams.java,v 1.6 2004/05/13 04:01:22 mbecke Exp $
+ * $Revision: 480424 $
+ * $Date: 2006-11-29 06:56:49 +0100 (Wed, 29 Nov 2006) $
+ *
+ * ====================================================================
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
 package org.apache.commons.httpclient.params;
 
 /**
@@ -5,12 +35,20 @@ package org.apache.commons.httpclient.params;
  * may be linked together to form a hierarchy. If a particular parameter value has not been
  * explicitly defined in the collection itself, its value will be drawn from the parent
  * collection of parameters.
+ *
+ * @author <a href="mailto:oleg@ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision: 480424 $
+ *
+ * @since 3.0
  */
 public interface HttpParams {
+
     /**
      * Returns the parent collection that this collection will defer to(听从)
      * for a default value if a particular parameter is not explicitly
      * set in the collection itself
+     *
      * @return the parent collection to defer to, if a particular parameter
      * is not explicitly set in the collection itself.
      *
@@ -190,4 +228,5 @@ public interface HttpParams {
      * <tt>false</tt> otherwise.
      */
     public boolean isParameterFalse(final String name);
+
 }
