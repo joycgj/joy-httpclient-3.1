137754060b4927b15d7056e3773b338de2c10a18
gaojiechen <gaojiechen@sohu-inc.com>
2014-01-04 22:32:28 +0800

    httpclient home

--- httpclient/src/main/java/org/apache/commons/httpclient/protocol/Protocol.java
+++ httpclient/src/main/java/org/apache/commons/httpclient/protocol/Protocol.java
@@ -1,31 +1,67 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//httpclient/src/java/org/apache/commons/httpclient/protocol/Protocol.java,v 1.10 2004/04/18 23:51:38 jsdever Exp $
+ * $Revision: 480424 $
+ * $Date: 2006-11-29 06:56:49 +0100 (Wed, 29 Nov 2006) $
+ *
+ * ====================================================================
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
 package org.apache.commons.httpclient.protocol;
-
-import org.apache.commons.httpclient.util.LangUtils;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.httpclient.util.LangUtils;
+
 /**
- * A class to encapsulate the specifics of a protocol. This class class also
- * provides the ability t customize the set and characteristics of the
+ * A class to encapsulate the specifics of a protocol.  This class class also
+ * provides the ability to customize the set and characteristics of the
  * protocols used.
  *
  * <p>One use case for modifying the default set of protocols would be to set a
- * custom SSL socket factory. This would look something like the following:
- * <pre>
- * Protocol myHTTPS = new Protocol("https", new MySSLSocketFactory(), 443);
+ * custom SSL socket factory.  This would look something like the following:
+ * <pre> 
+ * Protocol myHTTPS = new Protocol( "https", new MySSLSocketFactory(), 443 );
  *
- * Protocol.registerProtocol("https", myHTTPS);
+ * Protocol.registerProtocol( "https", myHTTPS );
  * </pre>
+ *
+ * @author Michael Becke 
+ * @author Jeff Dever
+ * @author <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
+ *
+ * @since 2.0
  */
-public class Protocol {
-     /** The available protocols */
+public class Protocol { // finish @2014/1/4 22:30
+
+    /** The available protocols */
     private static final Map PROTOCOLS = Collections.synchronizedMap(new HashMap());
 
     /**
-     * Registers a new protocol with the given identifier. If a protocol with
-     * the given ID already exists it will be overridden. This ID is the same
+     * Registers a new protocol with the given identifier.  If a protocol with
+     * the given ID already exists it will be overridden.  This ID is the same
      * one used to retrieve the protocol from getProtocol(String).
      *
      * @param id the identifier for this protocol
@@ -59,7 +95,17 @@ public class Protocol {
         PROTOCOLS.remove(id);
     }
 
-    public static Protocol getProtocol(String id) throws IllegalStateException {
+    /**
+     * Gets the protocol with the given ID.
+     *
+     * @param id the protocol ID
+     *
+     * @return Protocol a protocol
+     *
+     * @throws IllegalStateException if a protocol with the ID cannot be found
+     */
+    public static Protocol getProtocol(String id)
+            throws IllegalStateException {
 
         if (id == null) {
             throw new IllegalArgumentException("id is null");
@@ -77,13 +123,14 @@ public class Protocol {
     /**
      * Lazily registers the protocol with the given id.
      *
-     * @param id teh protocol ID
+     * @param id the protocol ID
      *
      * @return the lazily registered protocol
      *
      * @throws IllegalStateException if the protocol with id is not recognized
      */
-    private static Protocol lazyRegisterProtocol(String id) throws IllegalStateException {
+    private static Protocol lazyRegisterProtocol(String id)
+            throws IllegalStateException {
 
         if ("http".equals(id)) {
             final Protocol http
@@ -94,13 +141,14 @@ public class Protocol {
 
         if ("https".equals(id)) {
             final Protocol https
-                = new Protocol("https", SSLProtocolSocketFactory.getSocketFactory(), 443);
+                    = new Protocol("https", SSLProtocolSocketFactory.getSocketFactory(), 443);
             Protocol.registerProtocol("https", https);
             return https;
         }
 
         throw new IllegalStateException("unsupported protocol: '" + id + "'");
     }
+
 
     /** the scheme of this protocol (e.g. http, https) */
     private String scheme;
@@ -116,7 +164,7 @@ public class Protocol {
 
     /**
      * Constructs a new Protocol. Whether the created protocol is secure depends on
-     * the class of <code>factory</code>
+     * the class of <code>factory</code>.
      *
      * @param scheme the scheme (e.g. http, https)
      * @param factory the factory for creating sockets for communication using
@@ -143,7 +191,7 @@ public class Protocol {
 
     /**
      * Constructs a new Protocol. Whether the created protocol is secure depends on
-     * the class of <code>factory</code>
+     * the class of <code>factory</code>.
      *
      * @param scheme the scheme (e.g. http, https)
      * @param factory the factory for creating sockets for communication using
@@ -152,12 +200,13 @@ public class Protocol {
      * @deprecated Use the constructor that uses ProtocolSocketFactory, this version of
      * the constructor is only kept for backwards API compatibility.
      */
-    public Protocol(String scheme, SecureProtocolSocketFactory factory, int defaultPort) {
-        this(scheme, (ProtocolSocketFactory)factory, defaultPort);
+    public Protocol(String scheme,
+                    SecureProtocolSocketFactory factory, int defaultPort) {
+        this(scheme, (ProtocolSocketFactory) factory, defaultPort);
     }
 
     /**
-     * Returns the defaultPort
+     * Returns the defaultPort.
      * @return int
      */
     public int getDefaultPort() {
@@ -165,7 +214,7 @@ public class Protocol {
     }
 
     /**
-     * Returns the socketFactory. If secure the factory is a
+     * Returns the socketFactory.  If secure the factory is a
      * SecureProtocolSocketFactory.
      * @return SocketFactory
      */
@@ -174,7 +223,7 @@ public class Protocol {
     }
 
     /**
-     * Returns the scheme
+     * Returns the scheme.
      * @return The scheme
      */
     public String getScheme() {
@@ -190,7 +239,7 @@ public class Protocol {
     }
 
     /**
-     * Resolves the correct port for this protocol. Returns the given port if
+     * Resolves the correct port for this protocol.  Returns the given port if
      * valid or the default port otherwise.
      *
      * @param port the port to be resolved
@@ -202,12 +251,11 @@ public class Protocol {
     }
 
     /**
-     * Returns a string representation of this object.
+     * Return a string representation of this object.
      * @return a string representation of this object.
      */
-    @Override
     public String toString() {
-       return scheme + ":" + defaultPort;
+        return scheme + ":" + defaultPort;
     }
 
     /**
@@ -215,32 +263,33 @@ public class Protocol {
      * @param obj The object to compare against.
      * @return true if the objects are equal.
      */
-    @Override
     public boolean equals(Object obj) {
 
         if (obj instanceof Protocol) {
+
             Protocol p = (Protocol) obj;
 
             return (
-                 defaultPort == p.getDefaultPort()
-                 && scheme.equalsIgnoreCase(p.getScheme())
-                 && secure == p.isSecure()
-                 && socketFactory.equals(p.getSocketFactory()));
+                    defaultPort == p.getDefaultPort()
+                            && scheme.equalsIgnoreCase(p.getScheme())
+                            && secure == p.isSecure()
+                            && socketFactory.equals(p.getSocketFactory()));
+
         } else {
-           return false;
+            return false;
         }
+
     }
 
     /**
-     * Returns a hash code for this object.
+     * Return a hash code for this object
      * @return The hash code.
      */
-    @Override
     public int hashCode() {
         int hash = LangUtils.HASH_SEED;
         hash = LangUtils.hashCode(hash, this.defaultPort);
         hash = LangUtils.hashCode(hash, this.scheme.toLowerCase());
-        hash = LangUtils.hashCode(hash, this.scheme);
+        hash = LangUtils.hashCode(hash, this.secure);
         hash = LangUtils.hashCode(hash, this.socketFactory);
         return hash;
     }
